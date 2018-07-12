1d6fa1f23d4f32c9003bb98d377f105f81a339a8
sumitkumar <sumit@binarynumbers.io>
2018-07-12 19:12:40 +0530

    ''

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -138,7 +138,7 @@ router.post('/comment/:id',passport.authenticate('jwt',{session: false}),(req,re
             text: req.body.text,
             name: req.body.name,
             avatar: req.body.avatar,
-            user: req.body.user,
+            user: req.user.id,
         }
         // Add to comments array
         post.comments.unshift(newComment);
@@ -148,4 +148,29 @@ router.post('/comment/:id',passport.authenticate('jwt',{session: false}),(req,re
     .catch(err => res.status(404).json({postnotfound:'No post found'}));
 });
 
+//@route    Delete api/posts/Comment/:id/:comment_id
+//@desc     Remove comment from post
+//@access   private
+router.delete('/comment/:id/:comment_id',passport.authenticate('jwt',{session: false}),(req,res)=>{
+    debugger
+    Post.findById(req.params.id)
+    .then(post =>{
+
+        // Check to see if comments exists
+        if(post.comments.filter(comment => comment._id.toString()=== req.params.comment_id).length===0){
+            return res.status(404).json({commentnotexists:'Comment does not exist'});
+        }
+
+        //Get remove Index
+        const removeIndex = post.comments
+        .map(item => item._id.toString())
+        .indexOf(req.params.comment_id);
+
+        // Splice comment out of array
+        post.comments.splice(removeIndex,1)
+
+        post.save().then(post => res.json(post));
+    })
+    .catch(err => res.status(404).json({postnotfound:'No post found'}));
+});
 module.exports = router;
